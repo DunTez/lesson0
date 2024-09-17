05e8434f90f4d85aca339a51c39a709c197267df
DunTez <brizhik1998@gmail.com>
2024-09-17 15:16:29 +0200

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,8 @@
+# 1st Program
+print(9 ** 0.5 * 5)
+# 2st Program
+print(9.99 > 9.98 and 1000 != 1001)
+# 3st Program
+print(2 * 2 + 2 == 2 * (2 + 2))
+# 4st Program
+print((int((float('123.456')) *10)) % 10)
